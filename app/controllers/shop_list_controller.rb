a4cd4a30599a7dcdbea7892d51ddb9c2b2b3f8e9
Ricardo Leonel Acosta <ricardo.acosta1809@gmail.com>
2021-10-19 17:47:36 -0500

    Creates ShopLists controller

---
+++ app/controllers/shop_list_controller.rb
@@ -0,0 +1,16 @@
+class ShopListController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'shop_list/index'
+  get 'shop_list/create'
+  get 'shop_list/delete'
+  get 'shop_list/show'
+  get 'shop_list/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
